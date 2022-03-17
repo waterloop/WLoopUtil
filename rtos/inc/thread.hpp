9a02aa579bacd9e7acdaa12650b861e4a5df8644
Ryan Mah <rmah@uwaterloo.ca>
2022-03-17 16:01:10 -0400

    tries to make it so that thread objects don't need to be static anymore

--- rtos/inc/thread.hpp
+++ rtos/inc/thread.hpp
@@ -2,17 +2,32 @@
 #include <string.h>
 #include "cmsis_os.h"
 
+typedef void (*RTOSCallback)(void*);
+
 class RTOSThread {
     public:
         RTOSThread();
-        RTOSThread(char const * name, uint32_t stack_size, osPriority_t priority, void callback(void* arg));    
-        
+
+        // keeping this here to not break current builds,
+        // depreciate this once all repos have been migrated
+        RTOSThread(
+            char const * name_, uint32_t stack_size_, osPriority_t priority_, RTOSCallback callback_);
+
+        void create();
+
         void suspendThread();
         void resumeThread();
-        
+
         uint32_t setFlag(uint32_t flag);
 
         osThreadId_t getThreadId();    
-    private:
-        osThreadId_t thread;
+
+    protected:
+        osThreadId_t thread_;
+
+        const char* name;
+        uint32_t stack_size;
+        osPriority_t priority;
+        RTOSCallback callback;
+        void* callback_arg;
 };
--- rtos/src/thread.cpp
+++ rtos/src/thread.cpp
@@ -1,28 +1,32 @@
 #include "thread.hpp"
 
-RTOSThread::RTOSThread(char const * name, uint32_t stack_size, osPriority_t priority, void callback(void* arg)) {
-    const osThreadAttr_t thread_attrs = {
-        .name = name,
-        .stack_size = stack_size,
-        .priority = priority
-    };
+RTOSThread::RTOSThread() {}
+RTOSThread::RTOSThread(
+        char const * name_, uint32_t stack_size_, osPriority_t priority_, RTOSCallback callback_) {
 
-    this->thread = osThreadNew(
-        callback,
-        NULL,
-        &thread_attrs
-    );
-
+    this->name = name_;
+    this->stack_size = stack_size_;
+    this->priority = priority_;
+    this->callback = callback_;
+    this->callback_arg = nullptr;
+    this->create();
 }
 
-RTOSThread::RTOSThread() {}
+void RTOSThread::create() {
+    const osThreadAttr_t thread_attrs = {
+        .name = this->name,
+        .stack_size = this->stack_size,
+        .priority = this->priority
+    };
+    this->thread_ = osThreadNew(this->callback, (void*)this, &thread_attrs);
+}
 
 void RTOSThread::suspendThread() {
-    osThreadSuspend(this->thread);
+    osThreadSuspend(this->thread_);
 }
 
 void RTOSThread::resumeThread() {
-    osThreadResume(this->thread);
+    osThreadResume(this->thread_);
 }
 
 uint32_t RTOSThread::setFlag(uint32_t flag) {
@@ -30,6 +34,6 @@ uint32_t RTOSThread::setFlag(uint32_t flag) {
 }
 
 osThreadId_t RTOSThread::getThreadId() {
-    return this->thread;
+    return this->thread_;
 }
 
